9d16a770b68f2b6c2d40158961db9d5f32ab5536
D7702_09 <d7702_09@d7702-09ui-imac.local>
2019-04-17 15:42:58 +0900

    오늘의 날짜 코드 추가

--- DatePick/ViewController.swift
+++ DatePick/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var outLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        let  locale = Locale(identifier: "ko_KO")
+        myDatePicker.locale=locale
+        
+        myDatePicker.datePickerMode = UIDatePicker.Mode.dateAndTime
+        
+        let date = Date();
+        let format = DateFormatter();
+        format.dateFormat = "yyyy-MM-dd HH:mm EEE a"
+        let formatterDate = format.string(from: date)
+        outLabel.text = formatterDate
+        
+    
+        
     }
 
-
+    @IBAction func valueChange(_ sender: Any) {
+        let  selectedData = myDatePicker.date
+        
+        
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE a"
+        outLabel.text = formatter.string(from: selectedData)
+        
+        
+    }
+    
 }
 
